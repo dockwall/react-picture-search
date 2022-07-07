334b4dfedcda26b3a01c6b546551fc893fe7615b
Stas <leshuksu@gmail.com>
2022-07-07 19:46:14 +0300

    Added change handler to search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    onInputChange(evt) {
+        console.log(evt.target.value);
+    }
+
     render() {
         return (
             <div className='ui segment'>
                 <form className='ui form'>
                     <div className='field'>
                         <label>Search image by text</label>
-                        <input type='text' placeholder='Print your query' />
+                        <input type='text' placeholder='Print your query' onChange={this.onInputChange} />
                     </div>
                 </form>
             </div>
