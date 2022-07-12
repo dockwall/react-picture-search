02cb16aa6193bbfda053c835de891725d9f848c9
Stas <leshuksu@gmail.com>
2022-07-12 09:52:12 +0300

    Added change handler to search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,10 +5,15 @@ class SearchBar extends React.Component {
         term: '',
     };
 
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state.term)
+    }
+
     render() {
         return (
             <div className='ui segment'>
-                <form className='ui form'>
+                <form onSubmit={this.onFormSubmit} className='ui form'>
                     <div className='field'>
                         <label>Search image by text</label>
                         <input
