5da4b00f45caa0d78bdc814d8da617244bafdb69
Stas <leshuksu@gmail.com>
2022-10-12 14:20:20 +0300

    Added event handlers, prevented reload after submit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,25 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    onInputChange(evt) {
+        console.log(evt.target.value)
+    }
+
+    onFormSubmit(evt) {
+        evt.preventDefault();
+        console.log('Data was successfully submitted!')
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label>Enter phrase for search image</label>
-                        <input type="text"></input>
+                        <input type="text" onChange={this.onInputChange}></input>
                     </div>
-                </form>
-            </div>
+                </form >
+            </div >
         )
     }
 }
