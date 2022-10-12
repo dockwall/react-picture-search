94605a5d8450df5104b5d7abb788f2a252234466
Stas <leshuksu@gmail.com>
2022-10-12 18:06:18 +0300

    Refactored App.js. Started separating API from search bar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-    return (
-        <div className="ui container" style={{ marginTop: '15px' }}>
-            <SearchBar />
-        </div>
-    )
+class App extends React.Component {
+
+    onSearchSubmit(searchTerm) {
+        console.log(searchTerm);
+    }
+
+    render() {
+        return (
+            <div
+                className="ui container"
+                style={{ marginTop: '15px' }}
+            >
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    }
 };
 
 export default App;
