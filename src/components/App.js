c2b14d38ef2ae3baf5f1624d000aff67d1777b5a
Stas <leshuksu@gmail.com>
2022-10-14 01:50:52 +0300

    Used axios to request pictures url by text query

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import axios from 'axios';
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
 
     onSearchSubmit(searchTerm) {
-        console.log(searchTerm);
+        // EasterEgg: this is my unsplash public token
+        axios.get('https://api.unsplash.com/search/photos',
+            {
+                params: {
+                    query: searchTerm,
+                },
+
+                headers: {
+                    Authorization: 'Client-ID ulLTB12UZMQQ2WBv_MLqHA4aX5KljpsODsgDRbWPXDE',
+                },
+            }
+        )
     }
 
     render() {
