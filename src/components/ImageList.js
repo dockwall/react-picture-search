5389e245a5a5d2e3b7620c72b76e34c487135cec
Stas <leshuksu@gmail.com>
2022-10-16 14:23:20 +0300

    Refactored ImageList component. Added keys (id) for React performance

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,8 +2,14 @@ import React from "react";
 
 const ImageList = (props) => {
 
-    const images = props.images.map(imageObject => {
-        return <img src={imageObject.urls.regular} alt={imageObject.alt_description} />
+    const images = props.images.map(({ description, id, urls }) => {
+        return (
+            <img
+                src={urls.regular}
+                alt={description}
+                key={id}
+            />
+        )
     });
 
     return <div>{images}</div>;
