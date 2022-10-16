107dc45f96b3fd7facff70a07d28a12b087c6d0f
Stas <leshuksu@gmail.com>
2022-10-17 00:24:22 +0300

    Added React ref for all images to get their actual height (after render AND loading)

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,16 +1,11 @@
 import './ImageList.css';
+import ImageCard from './ImageCard';
 import React from "react";
 
 const ImageList = (props) => {
 
-    const images = props.images.map(({ description, id, urls }) => {
-        return (
-            <img
-                src={urls.regular}
-                alt={description}
-                key={id}
-            />
-        )
+    const images = props.images.map(image => {
+        return <ImageCard key={image.id} image={image} />
     });
 
     return <div className="image-list">{images}</div>;
