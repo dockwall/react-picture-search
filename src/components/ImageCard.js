239c42f21b6868a0ab1acc687c7d216b1d1155c8
Stas <leshuksu@gmail.com>
2022-10-17 17:23:46 +0300

    Played with grid CSS to render images list without space

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,18 +4,28 @@ class ImageCard extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = { spans: 0 };
+
         this.imageRef = React.createRef();
     }
 
     componentDidMount() {
-        this.imageRef.current.onload = () => (console.log(this.imageRef.current.clientHeight))
+        this.imageRef.current.onload = this.setSpans
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 10);
+
+        this.setState({ spans })
+
     }
 
     render() {
         const { description, urls } = this.props.image;
 
         return (
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
                 <img
                     ref={this.imageRef}
                     alt={description}
