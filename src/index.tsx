72627ad0ded0ab4efe71f0457e06fbea0bd0365f
Stas <leshuksu@gmail.com>
2023-03-26 23:05:54 +0300

    Transfered to TS

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const element = document.getElementById('root')!;
+
+const root = ReactDOM.createRoot(element);
 
 root.render(<App />);
