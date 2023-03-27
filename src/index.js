8961e29145b9118bc4422abddf1dce349332ef51
Stas <leshuksu@gmail.com>
2023-03-27 12:42:22 +0300

    Temporarily restored JS

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import './index.css'
 
-const element = document.getElementById('root')!;
+const element = document.getElementById('root');
 
 const root = ReactDOM.createRoot(element);
 
